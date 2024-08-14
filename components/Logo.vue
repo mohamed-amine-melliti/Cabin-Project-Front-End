c2505c49482d90b43b9b0a648dd638e7040ea974
Majdi77 <Majdi.melliti@esprit.tn>
2024-08-14 20:49:30 +0100

    first commit after project deletion

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+  big: { type: Boolean, required: false, default: false },
+})
+</script>
+<template>
+  <NuxtImg
+    src="/logo.png"
+    class="block"
+    :height="big ? '32' : '32'"
+    :width="big ? '32' : '32'"
+    alt="company logo" />
+</template>
---
+++ components/nav/Logo.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <NuxtLink
+    to="/"
+    class="flex items-center mr-auto gap-x-4 shrink-0">
+    <Logo />
+  </NuxtLink>
+</template>
